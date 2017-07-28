61614b53d12198b69541576b1923b9e6dfbb84e6
kevin <kevinweiqy@gmail.com>
2017-07-28 11:09:50 +0800

    newly add a sout method

---
+++ src/main/java/com/heitian/ssm/foundation/Java8_New_Features/LambdaExpression.java
@@ -0,0 +1,7 @@
+package com.heitian.ssm.foundation.Java8_New_Features;
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        System.out.println("Lambda Expression");
+    }
+}
